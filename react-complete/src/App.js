46e64a090a76c7506f9437fd6be54498e49b66e3
darthinvader <psaradakisj@gmail.com>
2021-07-03 12:36:39 +0300

    [learning] rendering elements with map

--- react-complete/src/App.js
+++ react-complete/src/App.js
@@ -52,22 +52,9 @@ class App extends Component {
     if (this.state.showPersons) {
       persons = (
         <div>
-          <Person
-            name={this.state.persons[0].name}
-            age={this.state.persons[0].age}
-          />
-          <Person
-            name={this.state.persons[1].name}
-            age={this.state.persons[1].age}
-            click={this.switchNameHandler.bind(this, "Max!")}
-            changed={this.nameChangedHandler}
-          >
-            My hoobbies: Racing
-          </Person>
-          <Person
-            name={this.state.persons[2].name}
-            age={this.state.persons[2].age}
-          />
+          {this.state.persons.map((person) => {
+            return <Person name={person.name} age={person.age} />;
+          })}
         </div>
       );
     }
@@ -77,7 +64,7 @@ class App extends Component {
         <h1>Hi I'm a react App</h1>
         <p>This is really working!</p>
         <button style={style} onClick={this.togglePersonsHandler}>
-          Switch Name
+          Toggle Persons
         </button>
         {persons}
       </div>
