ba75399dfd540ffe407df44122a723f2123a1b34
darthinvader <psaradakisj@gmail.com>
2021-07-03 13:15:58 +0300

    [learning] flexible lists

--- react-complete/src/App.js
+++ react-complete/src/App.js
@@ -19,13 +19,19 @@ class App extends Component {
     this.setState({ persons: persons });
   };
 
-  nameChangedHandler = (event) => {
+  nameChangedHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex((p) => {
+      return p.id === id;
+    });
+
+    const person = { ...this.state.persons[personIndex] };
+    person.name = event.target.value;
+
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+
     this.setState({
-      persons: [
-        { name: "Max", age: 28 },
-        { name: event.target.value, age: 29 },
-        { name: "Stephanie", age: 26 },
-      ],
+      persons,
     });
   };
 
@@ -54,6 +60,7 @@ class App extends Component {
                 name={person.name}
                 age={person.age}
                 key={person.id}
+                changed={(event) => this.nameChangedHandler(event, person.id)}
               />
             );
           })}
