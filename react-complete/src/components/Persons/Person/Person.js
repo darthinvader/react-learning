1af8ff2d083871476072e5bd374a6b1bba5dfd43
darthinvader <psaradakisj@gmail.com>
2021-07-07 21:48:30 +0300

    [learning] adjacent and hoc components

--- react-complete/src/components/Persons/Person/Person.js
+++ react-complete/src/components/Persons/Person/Person.js
@@ -1,13 +1,16 @@
 import React from "react";
 import Person from "./Person.css";
+import Aux from "../../../hoc/Auxiliary";
 
 const person = (props) => (
-  <div className={Person.Person}>
-    <p onClick={props.click}>
-      I'm {props.name} and I am {props.age} years old!
-    </p>
-    <p>{props.children}</p>
-    <input type="text" onChange={props.changed} value={props.name} />
-  </div>
+  <Aux>
+    <div className={Person.Person}>
+      <p onClick={props.click}>
+        I'm {props.name} and I am {props.age} years old!
+      </p>
+      <p>{props.children}</p>
+      <input type="text" onChange={props.changed} value={props.name} />
+    </div>
+  </Aux>
 );
 export default person;
