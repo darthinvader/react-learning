d34ef996ebfad0bcee8ba7d44aec4fad2ae54ac4
darthinvader <psaradakisj@gmail.com>
2021-07-07 16:28:59 +0300

    [learning] removing console logs

--- react-complete/src/components/Persons/Person/Person.js
+++ react-complete/src/components/Persons/Person/Person.js
@@ -1,17 +1,13 @@
 import React from "react";
 import Person from "./Person.css";
 
-const person = (props) => {
-  console.log("[Person.js] rendering...");
-  return (
-    <div className={Person.Person}>
-      <p onClick={props.click}>
-        I'm {props.name} and I am {props.age} years old!
-      </p>
-      <p>{props.children}</p>
-      <input type="text" onChange={props.changed} value={props.name} />
-    </div>
-  );
-};
-
+const person = (props) => (
+  <div className={Person.Person}>
+    <p onClick={props.click}>
+      I'm {props.name} and I am {props.age} years old!
+    </p>
+    <p>{props.children}</p>
+    <input type="text" onChange={props.changed} value={props.name} />
+  </div>
+);
 export default person;
