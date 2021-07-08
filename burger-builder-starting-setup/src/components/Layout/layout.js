a5bc564d4a61c90bc6710c0a83cc83dd37e28448
darthinvader <psaradakisj@gmail.com>
2021-07-08 19:09:18 +0300

    [learning] create-react-app update from old project

--- burger-builder-starting-setup/src/components/Layout/layout.js
+++ burger-builder-starting-setup/src/components/Layout/layout.js
@@ -1,8 +1,9 @@
 import React from "react";
+import classes from "./layout.module.css";
 const layout = (props) => (
   <React.Fragment>
     <div>Toolbar, SideDrawer,Backdrop</div>
-    <main>{props.children}</main>
+    <main className={classes.Content}>{props.children}</main>
   </React.Fragment>
 );
 
