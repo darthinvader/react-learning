a93d3b445cfd8da99b82ce3201204852bf2ffbd3
darthinvader <psaradakisj@gmail.com>
2021-07-08 13:52:38 +0300

    [learning] Passing uknown props

--- react-complete/src/components/Persons/Person/Person.js
+++ react-complete/src/components/Persons/Person/Person.js
@@ -1,16 +1,15 @@
 import React from "react";
 import Person from "./Person.css";
+import withClassDifferent from "../../../hoc/WithClassDifferent";
 import Aux from "../../../hoc/Auxiliary";
 
 const person = (props) => (
   <React.Fragment>
-    <div className={Person.Person}>
-      <p onClick={props.click}>
-        I'm {props.name} and I am {props.age} years old!
-      </p>
-      <p>{props.children}</p>
-      <input type="text" onChange={props.changed} value={props.name} />
-    </div>
+    <p onClick={props.click}>
+      I'm {props.name} and I am {props.age} years old!
+    </p>
+    <p>{props.children}</p>
+    <input type="text" onChange={props.changed} value={props.name} />
   </React.Fragment>
 );
-export default person;
+export default withClassDifferent(person, Person.Person);
--- react-complete/src/hoc/WithClassDifferent.js
+++ react-complete/src/hoc/WithClassDifferent.js
@@ -4,7 +4,7 @@ import React from "react";
 const withClassDifferent = (WrappedComponent, className) => {
   return (props) => (
     <div className={className}>
-      <WrappedComponent />
+      <WrappedComponent {...props} />
     </div>
   );
 };
