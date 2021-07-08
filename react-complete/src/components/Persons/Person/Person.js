40f4909cb43499cd58970e2492353a64e9d5ec97
darthinvader <psaradakisj@gmail.com>
2021-07-08 14:13:44 +0300

    [learning] ref

--- react-complete/src/components/Persons/Person/Person.js
+++ react-complete/src/components/Persons/Person/Person.js
@@ -1,17 +1,30 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Person from "./Person.css";
 import withClassDifferent from "../../../hoc/WithClassDifferent";
 import PropTypes from "prop-types";
 
-const person = (props) => (
-  <React.Fragment>
-    <p onClick={props.click}>
-      I'm {props.name} and I am {props.age} years old!
-    </p>
-    <p>{props.children}</p>
-    <input type="text" onChange={props.changed} value={props.name} />
-  </React.Fragment>
-);
+const person = (props) => {
+  useEffect(() => {
+    this.inputElement.focus();
+  }, []);
+
+  return (
+    <React.Fragment>
+      <p onClick={props.click}>
+        I'm {props.name} and I am {props.age} years old!
+      </p>
+      <p>{props.children}</p>
+      <input
+        type="text"
+        ref={(inputEl) => {
+          this.inputElement = inputEl;
+        }}
+        onChange={props.changed}
+        value={props.name}
+      />
+    </React.Fragment>
+  );
+};
 
 person.propTypes = {
   click: PropTypes.func,
