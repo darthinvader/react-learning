b4a8777ab0ff4b42fd4ce41f180a44a33a01d880
darthinvader <psaradakisj@gmail.com>
2021-07-09 16:18:26 +0300

    [learning] http request from axios

--- http-ajax/src/containers/Blog/Blog.js
+++ http-ajax/src/containers/Blog/Blog.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useEffect } from "react";
+import axios from "axios";
 
 import Post from "../../components/Post/Post";
 import FullPost from "../../components/FullPost/FullPost";
@@ -6,6 +7,12 @@ import NewPost from "../../components/NewPost/NewPost";
 import "./Blog.css";
 
 const Blog = () => {
+  useEffect(() => {
+    axios.get("https://jsonplaceholder.typicode.com/posts").then((response) => {
+      console.log(response);
+    });
+  }, []);
+
   return (
     <div>
       <section className="Posts">
