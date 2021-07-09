7e416c098cefecd98fae5cbe43031bc13861071a
darthinvader <psaradakisj@gmail.com>
2021-07-09 16:26:39 +0300

    [learning] data to article posts

--- http-ajax/src/containers/Blog/Blog.js
+++ http-ajax/src/containers/Blog/Blog.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 
 import Post from "../../components/Post/Post";
@@ -7,19 +7,22 @@ import NewPost from "../../components/NewPost/NewPost";
 import "./Blog.css";
 
 const Blog = () => {
+  const [posts, setPosts] = useState([]);
+
   useEffect(() => {
     axios.get("https://jsonplaceholder.typicode.com/posts").then((response) => {
-      console.log(response);
+      setPosts(response.data);
+      // console.log(response);
     });
   }, []);
 
+  const postElements = posts.map((post) => {
+    return <Post key={post.id} title={post.title} />;
+  });
+
   return (
     <div>
-      <section className="Posts">
-        <Post />
-        <Post />
-        <Post />
-      </section>
+      <section className="Posts">{postElements}</section>
       <section>
         <FullPost />
       </section>
