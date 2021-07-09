ea4c31161c7079b351005cac5499d92f6966cf2a
darthinvader <psaradakisj@gmail.com>
2021-07-09 13:19:52 +0300

    [learning] connecting state to build controls

--- burger-builder-starting-setup/src/containers/BurgerBuilder/BurgerBuilder.js
+++ burger-builder-starting-setup/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -2,6 +2,13 @@ import React, { Fragment, useState } from "react";
 import Burger from "../../components/Burger/Burger";
 import BuildControls from "../../components/Burger/BuildControls/BuildControls";
 
+const INGREDIENT_PRICES = {
+  salad: 0.5,
+  cheese: 0.4,
+  meat: 1.3,
+  bacon: 0.7,
+};
+
 const BurgerBuilder = (props) => {
   const [ingredients, setIngredients] = useState({
     salad: 0,
@@ -10,10 +17,24 @@ const BurgerBuilder = (props) => {
     cheese: 0,
   });
 
+  const [totalPrice, setTotalPrice] = useState(4);
+
+  const addIngredientHandler = (type) => {
+    const updatedCount = ingredients[type] + 1;
+    const updatedIngredients = {
+      ...ingredients,
+    };
+    updatedIngredients[type] = updatedCount;
+    setIngredients(updatedIngredients);
+
+    const priceAddition = INGREDIENT_PRICES[type];
+    setTotalPrice(totalPrice + priceAddition);
+  };
+  const removeIngridientHandler = (type) => {};
   return (
     <Fragment>
       <Burger ingredients={ingredients} />
-      <BuildControls />
+      <BuildControls ingredientAdded={addIngredientHandler} />
     </Fragment>
   );
 };
