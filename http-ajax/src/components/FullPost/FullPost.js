423786089463ad34b505f15c4a8e37204e9de093
darthinvader <psaradakisj@gmail.com>
2021-07-09 17:10:58 +0300

    [learning] Fetching data on update

--- http-ajax/src/components/FullPost/FullPost.js
+++ http-ajax/src/components/FullPost/FullPost.js
@@ -1,14 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 import "./FullPost.css";
 
 const FullPost = (props) => {
+  const [loadedPost, setLoadedPost] = useState(null);
+  useEffect(() => {
+    if (props.id) {
+      axios
+        .get(`https://jsonplaceholder.typicode.com/posts/${props.id}`)
+        .then((response) => {
+          // console.log(response);
+          setLoadedPost(response.data);
+        });
+    }
+  }, [props]);
+
   let post = <p style={{ textAlign: "center" }}>Please select a Post!</p>;
   if (props.id) {
+    post = <p style={{ textAlign: "center" }}>loading...</p>;
+  }
+  if (loadedPost) {
     post = (
       <div className="FullPost">
-        <h1>Title</h1>
-        <p>Content</p>
+        <h1>{loadedPost.title}</h1>
+        <p>{loadedPost.body}</p>
         <div className="Edit">
           <button className="Delete">Delete</button>
         </div>
