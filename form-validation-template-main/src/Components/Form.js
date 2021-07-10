8a2a5d566fdce071cfa64cd0634c58a486f8dab5
darthinvader <psaradakisj@gmail.com>
2021-07-10 17:17:50 +0300

    [learning] yup and useForm

--- form-validation-template-main/src/Components/Form.js
+++ form-validation-template-main/src/Components/Form.js
@@ -4,22 +4,71 @@ import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
 
+const schema = yup.object().shape({
+  firstName: yup.string().required(),
+  lastName: yup.string().required(),
+  email: yup.string().email().required(),
+  age: yup.number().positive().integer().required(),
+  password: yup.string().min(4).max(15).required(),
+  confirmPassword: yup.string().oneOf([yup.ref("password"), null]),
+});
+
 function Form() {
+  const { register, handleSubmit, errors } = useForm({
+    resolver: yupResolver(schema),
+  });
+
+  const submitForm = (data) => {
+    console.log(data);
+  };
+
   return (
     <div className="Form">
       <div className="title">Sign Up</div>
       <div className="inputs">
-        <form>
-          <input type="text" name="firstName" placeholder="First Name..." />
-          <input type="text" name="lastName" placeholder="Last Name..." />
-          <input type="text" name="email" placeholder="Email..." />
-          <input type="text" name="age" placeholder="Age..." />
-          <input type="text" name="password" placeholder="Password..." />
+        <form onSubmit={handleSubmit(submitForm)}>
+          <input
+            type="text"
+            name="firstName"
+            placeholder="First Name..."
+            ref={register}
+          />
+          <p>{errors.firstName?.message}</p>
+          <input
+            type="text"
+            name="lastName"
+            placeholder="Last Name..."
+            ref={register}
+          />
+          <p>{errors.lastName?.message}</p>
+          <input
+            type="text"
+            name="email"
+            placeholder="Email..."
+            ref={register}
+          />
+          <p>{errors.email?.message}</p>
+          <input
+            type="text"
+            name="age"
+            placeholder="Age..."
+            ref={register}
+          />{" "}
+          <p>{errors.age?.message}</p>
+          <input
+            type="text"
+            name="password"
+            placeholder="Password..."
+            ref={register}
+          />
+          <p>{errors.password?.message}</p>
           <input
             type="text"
             name="confirmPassword"
             placeholder="Password again..."
+            ref={register}
           />
+          <p>{errors.confirmPassword && "Password should match"}</p>
           <input type="submit" id="submit" />
         </form>
       </div>
