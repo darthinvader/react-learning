ff0668887fb8f8c76c4fb9e6043a911a48ecd208
darthinvader <psaradakisj@gmail.com>
2021-07-11 20:35:56 +0300

    immutableset

--- redux-starter/src/index.js
+++ redux-starter/src/index.js
@@ -13,4 +13,10 @@ import { compose, pipe } from "lodash/fp";
 import { Map } from "immutable";
 let book = Map({ title: "Harry Potter" });
 
+function publish(book) {
+  return book.set("isPublished", true);
+}
+
+book = publish(book);
+
 console.log(book.toJS());
