2c99630c165dd78e93f34b0034d7f56637784afa
darthinvader <psaradakisj@gmail.com>
2021-07-11 21:11:01 +0300

    [learning] subscribing to store

--- redux-starter/src/index.js
+++ redux-starter/src/index.js
@@ -1,11 +1,17 @@
 import store from "./store";
 
+const unsubscribe = store.subscribe(() => {
+  console.log("store changed!", store.getState());
+});
+
 store.dispatch({
   type: "bugAdded",
   payload: {
     description: "BUG1",
   },
 });
+
+unsubscribe();
 
 store.dispatch({
   type: "bugRemove",
