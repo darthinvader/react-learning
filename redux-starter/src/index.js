ceeb9e96ad32328417ddddfb6bc39d7c041e056e
darthinvader <psaradakisj@gmail.com>
2021-07-11 20:34:19 +0300

    immutable

--- redux-starter/src/index.js
+++ redux-starter/src/index.js
@@ -1,11 +1,16 @@
 import { compose, pipe } from "lodash/fp";
 
-let input = "   javascript   ";
-let output = "<div>" + input.trim() + "</div>";
+// let input = "   javascript   ";
+// let output = "<div>" + input.trim() + "</div>";
 
-const trim = (str) => str.trim();
-const wrap = (type, str) => `<${type}>${str}<${type}>`;
-const toLowerCase = (str) => str.toLowerCase();
+// const trim = (str) => str.trim();
+// const wrap = (type, str) => `<${type}>${str}<${type}>`;
+// const toLowerCase = (str) => str.toLowerCase();
 
-const transform = pipe(trim, toLowerCase, wrapInDiv);
-transform(input);
+// const transform = pipe(trim, toLowerCase, wrapInDiv);
+// transform(input);
+
+import { Map } from "immutable";
+let book = Map({ title: "Harry Potter" });
+
+console.log(book.get("title"));
