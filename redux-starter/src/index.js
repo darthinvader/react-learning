00149713ae0384e068d599f339330189fc5e50f6
darthinvader <psaradakisj@gmail.com>
2021-07-12 19:41:10 +0300

    [learning] dispatching functions

--- redux-starter/src/index.js
+++ redux-starter/src/index.js
@@ -8,19 +8,19 @@ import {
 } from "./store/bugs";
 import { userAdded } from "./store/users";
 
-const store = configureStore();
+// const store = configureStore();
 
-store.dispatch(bugAdded({ description: "Bug1" }));
-store.dispatch(bugAdded({ description: "Bug2" }));
-store.dispatch(bugAdded({ description: "Bug3" }));
-store.dispatch(bugResolved({ id: 3 }));
-store.dispatch(userAdded({ name: "User 1" }));
-store.dispatch(userAdded({ name: "User 2" }));
-store.dispatch(bugAssignedToUser({ bugId: 1, userId: 2 }));
-store.dispatch(projectAdded({ name: "New Project" }));
+// store.dispatch(bugAdded({ description: "Bug1" }));
+// store.dispatch(bugAdded({ description: "Bug2" }));
+// store.dispatch(bugAdded({ description: "Bug3" }));
+// store.dispatch(bugResolved({ id: 3 }));
+// store.dispatch(userAdded({ name: "User 1" }));
+// store.dispatch(userAdded({ name: "User 2" }));
+// store.dispatch(bugAssignedToUser({ bugId: 1, userId: 2 }));
+// store.dispatch(projectAdded({ name: "New Project" }));
 
-console.log(store.getState());
-console.log(getBugsByUser(2)(store.getState()));
+// console.log(store.getState());
+// console.log(getBugsByUser(2)(store.getState()));
 
 // example without redux toolkit
 // import { createStore, applyMiddleware } from "redux";
@@ -28,3 +28,14 @@ console.log(getBugsByUser(2)(store.getState()));
 // import logger from "./store/middleware/logger";
 
 // const store = createStore(reducer, applyMiddleware(logger));
+
+// Dispatching Functions
+const store = configureStore();
+
+store.dispatch((dispatch, getState) => {
+  // Call an API
+  // When promise is resolved => dispatch()
+  dispatch({ type: "bug", bugs: [1, 2, 3] });
+  console.log(getState());
+  // If promise is Rejected => dispatch()
+});
--- redux-starter/src/store/configureStore.js
+++ redux-starter/src/store/configureStore.js
@@ -1,8 +1,9 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
 import reducer from "./reducer";
 import logger from "./middleware/logger";
+
 export default () =>
   configureStore({
     reducer,
-    middleware: [logger("console")],
+    middleware: [...getDefaultMiddleware(), logger("console")],
   });
