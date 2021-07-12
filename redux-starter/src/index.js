64b1848d7454049860bc622fa8771cef8c801943
darthinvader <psaradakisj@gmail.com>
2021-07-12 15:31:38 +0300

    [learning] Creating slices as exercise

--- redux-starter/src/index.js
+++ redux-starter/src/index.js
@@ -1,17 +1,20 @@
 import configureStore from "./store/configureStore";
-import * as actions from "./store/bugs";
+import * as actions from "./store/project";
 import { createStore } from "redux";
 
 const store = configureStore();
 
-const unsubscribe = store.subscribe(() => {
-  console.log("store changed!", store.getState());
-});
+// const unsubscribe = store.subscribe(() => {
+//   console.log("store changed!", store.getState());
+// });
 
-store.dispatch(actions.bugAdded({ description: "Bug1" }));
-store.dispatch(actions.bugAdded({ description: "Bug2" }));
-store.dispatch(actions.bugAdded({ description: "Bug3" }));
-store.dispatch(actions.bugResolved({ id: 3 }));
-console.log(store.getState());
+// store.dispatch(actions.bugAdded({ description: "Bug1" }));
+// store.dispatch(actions.bugAdded({ description: "Bug2" }));
+// store.dispatch(actions.bugAdded({ description: "Bug3" }));
+// store.dispatch(actions.bugResolved({ id: 3 }));
+// console.log(store.getState());
 
+// console.log(store.getState());
+
+store.dispatch(actions.projectAdded({ name: "New Project" }));
 console.log(store.getState());
