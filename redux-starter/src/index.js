1916a2616f5d67408db86907c0539bc83858dab0
darthinvader <psaradakisj@gmail.com>
2021-07-12 13:24:08 +0300

    [learning] redux dev tools

--- redux-starter/src/index.js
+++ redux-starter/src/index.js
@@ -1,15 +1,15 @@
-// import store from "./store";
-// import { bugAdded, bugResolved } from "./actions";
-// const unsubscribe = store.subscribe(() => {
-//   console.log("store changed!", store.getState());
-// });
+import store from "./store";
+import { bugAdded, bugResolved } from "./actions";
+const unsubscribe = store.subscribe(() => {
+  console.log("store changed!", store.getState());
+});
 
-// store.dispatch(bugAdded("Bug1"));
-// console.log(store.getState());
+store.dispatch(bugAdded("Bug1"));
+console.log(store.getState());
 // store.dispatch(bugResolved(1));
 
-// console.log(store.getState());
-// // unsubscribe();
+console.log(store.getState());
+// unsubscribe();
 
 // store.dispatch({
 //   type: "bugRemove",
@@ -20,12 +20,12 @@
 
 // console.log(store.getState());
 
-import store from "./customStore";
-import * as actions from "./actions";
-store.subscribe(() => {
-  console.log("store changed");
-});
-store.dispatch(actions.bugAdded("Bug 1"));
-store.dispatch(actions.bugAdded("Bug 2"));
+// import store from "./customStore";
+// import * as actions from "./actions";
+// store.subscribe(() => {
+//   console.log("store changed");
+// });
+// store.dispatch(actions.bugAdded("Bug 1"));
+// store.dispatch(actions.bugAdded("Bug 2"));
 
-console.log(store.getState());
+// console.log(store.getState());
