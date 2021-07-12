4a5bdcf5a85d8823d0aa69dbd431e1600238f0fa
darthinvader <psaradakisj@gmail.com>
2021-07-12 15:26:53 +0300

    [learning] Creating slices with redux toolkit

--- redux-starter/src/store/bugs.js
+++ redux-starter/src/store/bugs.js
@@ -1,26 +1,24 @@
-import { createAction, createReducer } from "@reduxjs/toolkit";
-const bugUpdated = createAction("bugUpdated");
-
-// Action creators
-export const bugAdded = createAction("bugAdded");
-export const bugRemoved = createAction("bugRemoved");
-
-export const bugResolved = createAction("bugResolved");
-
-// Reducer
+import { createSlice } from "@reduxjs/toolkit";
 let lastId = 0;
 
-export default createReducer([], {
-  [bugAdded.type]: (bugs, action) => {
-    bugs.push({
-      id: ++lastId,
-      description: action.payload.description,
-      resolved: false,
-    });
-  },
-
-  [bugResolved.type]: (bugs, action) => {
-    const index = bugs.findIndex((bug) => bug.id === action.payload.id);
-    bugs[index].resolved = true;
+const slice = createSlice({
+  name: "bugs",
+  initialState: [],
+  reducers: {
+    bugAdded: (bugs, action) => {
+      bugs.push({
+        id: ++lastId,
+        description: action.payload.description,
+        resolved: false,
+      });
+    },
+    bugResolved: (bugs, action) => {
+      const index = bugs.findIndex((bug) => bug.id === action.payload.id);
+      bugs[index].resolved = true;
+    },
   },
 });
+console.log(slice);
+
+export const { bugAdded, bugResolved } = slice.actions;
+export default slice.reducer;
