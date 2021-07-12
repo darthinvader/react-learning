803caccd5b2e6bc08292eaa625398910abf12659
darthinvader <psaradakisj@gmail.com>
2021-07-12 14:57:24 +0300

    [learning] fixed store removed

--- redux-starter/src/store/configureStore.js
+++ redux-starter/src/store/configureStore.js
@@ -1,6 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
 import reducer from "./bugs";
+
 export default () =>
-  (store = configureStore({
+  configureStore({
     reducer,
-  }));
+  });
